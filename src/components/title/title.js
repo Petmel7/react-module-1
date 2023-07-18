5af0a0c2a772260dbfdb9367e5684a4f7f113599
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-07-18 18:30:39 +0300

    add

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,17 +1,26 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import s from "./title.module.css";
+
+const styleForTitle = {
+  color: "green",
+  textTransform: "uppercase",
+};
 
 const Title = ({ value }) => {
-    return (
-        <h2>{ value }</h2>
-    )
-}
+  return (
+    <div className={s.titleWrapper}>
+      <h2 className={s.title} style={styleForTitle}>
+        {value}
+      </h2>
+    </div>
+  );
+};
 
-export default Title
+export default Title;
 Title.defaultProps = {
-    value: 'title'
-}
+  value: "title",
+};
 
 Title.propTypes = {
-    value: PropTypes.string,
-}
-
+  value: PropTypes.string,
+};
