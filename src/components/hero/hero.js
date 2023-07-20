4306e27647064d58988fee2f546fa61803687a8a
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-07-20 16:37:43 +0300

    add

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,7 +1,23 @@
+import images1 from "../../images/images1.jpg";
+import images2 from "../../images/images2.jpg";
+import images3 from "../../images/images3.jpg";
+import images4 from "../../images/images4.jpg";
+// import { Icons } from '../icons'
+
 export const Hero = () => {
   return (
     <>
-      <img src="" alt="" />
+      {/* <div>
+        <a>
+          <Icons name="spotify-icon" className="myFistSvg"/>
+        </a>
+      </div> */}
+      <div>
+        <img src={images1} alt="img" width={300} />
+        <img src={images2} alt="img" width={300} />
+        <img src={images3} alt="img" width={300} />
+        <img src={images4} alt="img" width={300} />
+      </div>
     </>
   );
 };
---
+++ src/components/icons.js
@@ -0,0 +1,13 @@
+// import svg from '../images/icons/sprite.svg'
+
+// export const Icons = ({ name, className, color, stroke, width, height }) => {
+//     return (
+//         <svg className={`icon icon-${name} ${className}`}
+//             fill={color}
+//             stroke={stroke}
+//             width={width}
+//             height={height}>
+//             <use xlinkHref={`${svg} #${name}`}></use>
+//         </svg>
+//     )
+// }
